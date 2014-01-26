6484a80cd2a4647f63fae5671d066dda37d64716
GC92 <gc92@ya.ru>
2014-01-27 03:03:45 +0700

    Updated to v0.3.0. :rocket:

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,35 @@
 module.exports = function (grunt) {
 	
 	grunt.initConfig({
-		
-		concat: {
-			dist: {
-				src: [
-					'src/js/libs/jquery-2.1.0.min.js',
-					'src/js/main.js'
-				],
-				dest: 'dest/js/main.min.js'
+
+		sprite: {
+			all: {
+				src: 'src/img/sprite/*.png',
+				destImg: 'src/img/sprite.png',
+				cssFormat: 'stylus',
+				destCSS: 'src/stylus/sprite.styl',
+				algorithm: 'binary-tree',
+				padding: 8,
+				engine: 'pngsmith',
+				imgOpts: {
+					format: 'png'
+				}
 			}
 		},
 
-		uglify: {
-			build: {
-				src: 'dest/js/main.min.js',
-				dest: 'dest/js/main.min.js'
-			}
-		},
-
-		jade: {
-			compile: {
+		imagemin: {
+			options: {
+				optimizationLevel: 1,
+				pngquant: true,
+				progressive: false,
+				interlaced: false
+			},
+			images: {
 				files: [{
-					cwd: 'src',
-					src: ['**/*.jade', '!inc/**/*.jade'],
-					dest: 'dest',
 					expand: true,
-					ext: '.html'
+					cwd: 'src/img/',
+					src: ['**/*.{png,jpg,gif}', '!sprite/*'],
+					dest: 'dest/img/'
 				}]
 			}
 		},
@@ -69,6 +72,42 @@ module.exports = function (grunt) {
 			}
 		},
 
+
+		jade: {
+			compile: {
+				files: [{
+					cwd: 'src',
+					src: ['**/*.jade', '!inc/**/*.jade'],
+					dest: 'dest',
+					expand: true,
+					ext: '.html'
+				}]
+			}
+		},
+		
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dest: {
+				src: [
+					'src/js/libs/jquery-2.1.0.min.js',
+					'src/js/main.js'
+				],
+				dest: 'dest/js/main.min.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				report: 'min'
+			},
+			build: {
+				src: 'dest/js/main.min.js',
+				dest: 'dest/js/main.min.js'
+			}
+		},
+
 		copy: {
 			fonts: {
 				files: [{
@@ -77,17 +116,6 @@ module.exports = function (grunt) {
 					src: '*',
 					dest: 'dest/fonts/',
 					filter: 'isFile'
-				}]
-			}
-		},
-
-		imagemin: {
-			images: {
-				files: [{
-					expand: true,
-					cwd: 'src/img/',
-					src: ['**/*.{png,jpg,gif}'],
-					dest: 'dest/img/'
 				}]
 			}
 		},
@@ -102,56 +130,62 @@ module.exports = function (grunt) {
 		},
 
 		watch: {
-			livereload: {
-				options: {
-					livereload: true
-				},
-				files: ['dest/**/*']
+			sprite: {
+				files: ['src/img/sprite/*.png'],
+				tasks: ['sprite', 'imagemin']
 			},
-			js: {
-				files: ['src/js/**/*.js'],
-				tasks: ['concat', 'uglify']
-			},
-			jade: {
-				files: ['src/**/*.jade', '!inc/**/*.jade'],
-				tasks: ['jade']
+			imagemin: {
+				files: ['src/img/**/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
 			},
 			stylus: {
 				files: ['src/stylus/**/*.styl'],
 				tasks: ['stylus', 'autoprefixer', 'cssmin']
 			},
+			jade: {
+				files: ['src/**/*.jade', '!inc/**/*.jade'],
+				tasks: ['jade']
+			},
+			js: {
+				files: ['src/js/**/*.js'],
+				tasks: ['concat', 'uglify']
+			},
 			copy: {
 				files: ['src/fonts/**/*'],
 				tasks: ['copy']
 			},
-			imagemin: {
-				files: ['src/img/**/*.{png,jpg,gif}'],
-				tasks: ['imagemin']
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: ['dest/**/*']
 			}
 		}
 
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-spritesmith');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-stylus');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
-		'concat',
-		'uglify',
-		'jade',
+		'sprite',
+		'imagemin',
 		'stylus',
 		'autoprefixer',
 		'cssmin',
+		'jade',
+		'concat',
+		'uglify',
 		'copy',
-		'imagemin',
 		'connect',
 		'watch'
 	]);
