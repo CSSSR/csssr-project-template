9a1d0b14551145497c9842d5851d9e3aeb185e80
Leonard Kinday <leonard@kinday.ru>
2015-03-30 18:08:48 +0300

    feat(wheel-filter): add

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -7,4 +7,37 @@ $(function () {
 	};
 
 	$('select').select2(SELECT2_OPTIONS);
+
+	// wheel filter
+	(function () {
+		var
+		$wf = $('.js-wf'),
+		$tabs = $('.js-wf-tab', $wf),
+		$bodies = $('.js-wf-body', $wf),
+		$discs = $('.js-rear-discs', $wf),
+		$tires = $('.js-rear-tires', $wf);
+
+		$discs.add($tires)
+			.not('[data-hide-on-init="false"]')
+			.hide();
+
+		$tabs.on('click', function (e) {
+			var
+			$target = $(e.target).closest('.js-wf-tab'),
+			id = $target.attr('data-id');
+
+			$tabs.removeClass('wf__tab_active');
+			$target.addClass('wf__tab_active');
+
+			$bodies.removeClass('wf-body_active');
+			$('.js-wf-body_' + id, $wf).addClass('wf-body_active');
+		});
+
+		$('[data-toggle]', $wf).on('click', function (e) {
+			var group = $(e.target).closest('[data-toggle]').attr('data-toggle');
+			console.log(group);
+			console.log($(group, $wf));
+			$(group, $wf).toggle();
+		});
+	})();
 });
