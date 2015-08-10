b85cd27fde64fe3cc3ba84ff1e9e63e4ecf8ae49
Aleksandr Petrov <dzhiriki@gmail.com>
2015-08-10 16:51:02 +0300

    fix(script): update codestyle

--- app/scripts/debug.js
+++ app/scripts/debug.js
@@ -2,10 +2,10 @@
 // in debug mode on the local server
 export default function () {
 	if (/localhost|127\.0\.0\.1|192\.168\.\d{1,3}\.\d{1,3}/.test(window.location.hostname)) {
-		var console = window.console;
-		var document = window.document;
-		var favicon = document.createElement('link');
-		var titleText = 'DEBUG — ' + document.title;
+		const console = window.console;
+		const document = window.document;
+		const favicon = document.createElement('link');
+		const titleText = 'DEBUG — ' + document.title;
 
 		favicon.rel = 'icon';
 		favicon.href = '/debug.ico';
@@ -13,6 +13,8 @@ export default function () {
 
 		document.getElementsByTagName('head')[0].appendChild(favicon);
 
-		console && console.info && console.info(titleText);
+		if (console && console.info) {
+			console.info(titleText);
+		}
 	}
 }
