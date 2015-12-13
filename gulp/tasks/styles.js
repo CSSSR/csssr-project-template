aff612c936588a4e3d57d0bbd84c5db32649337a
Felix Exter <felixexter@ya.ru>
2015-12-13 07:26:50 +0300

    fix(gcmq): rename cmq to gcmq

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,7 +5,7 @@ import gulpif from 'gulp-if';
 import rupture from 'rupture';
 import stylus from 'gulp-stylus';
 import autoprefixer from 'autoprefixer-stylus';
-import cmq from 'gulp-group-css-media-queries';
+import gcmq from 'gulp-group-css-media-queries';
 import minifyCss from 'gulp-minify-css';
 import csscomb from 'gulp-csscomb';
 import rename from 'gulp-rename';
@@ -29,7 +29,7 @@ gulp.task('styles', () => (
 				inline: true
 			} : false
 		}))
-		.pipe(gulpif(!gutil.env.debug, cmq()))
+		.pipe(gulpif(!gutil.env.debug, gcmq()))
 		.pipe(gulpif(!gutil.env.debug, minifyCss()))
 		.pipe(gulpif(gutil.env.csscomb, csscomb()))
 		.pipe(rename({suffix: '.min'}))
